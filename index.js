2c8c65b6beb9401b19dec4dff47db10240c8d774
Sergio G <soukron@gmbros.net>
2017-08-20 23:15:41 +0000

    Arreglos para meetup

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+// set render engine
+app.engine('html', require('ejs').renderFile);
+
+// offer static files under public/
+app.use(express.static(__dirname + '/public'));
+
+// health check
+app.get('/health', function(req, res) {
+  res.sendStatus(200);
 });
 
+// Send index
+app.get('/', function(req, res){
+  res.render(__dirname + '/index.html', 
+    { btncolor: process.env.BCOLOR || 'rgb(130, 224, 255)',
+      servername: process.env.HOSTNAME
+    }
+  );
+});
+
+// Create sockets
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 });
 
+// Startup server
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
